7bb0af86831a3b6ae273c5aecc78d4f7484d9794
codeameng <codeameng@gmail.com>
2025-02-24 11:05:25 +0800

    feat: add rule disabling for overridden TypeScript ESLint rules
    Enhance TypeScript ESLint configuration by:
    - Creating `disableOverriddenRules` function to automatically disable JavaScript rules that are overridden by TypeScript ESLint
    - Dynamically identify and turn off conflicting rules using Remeda functional utilities
    - Integrate the new configuration into TypeScript ESLint config generation

--- packages/eslint-config/src/configs/typescript.ts
+++ packages/eslint-config/src/configs/typescript.ts
@@ -1,6 +1,20 @@
 import { Config } from '$types/index.ts';
 import { defineConfig } from '$utils/index.ts';
 import typescriptEslint from 'typescript-eslint';
+import eslintJs from '@eslint/js';
+import { R } from '@packages/utils';
+
+function disableOverriddenRules(): Config {
+  return {
+    name: 'typescript-disable-overridden-rules',
+    rules: R.pipe(
+      eslintJs.configs.all.rules,
+      R.keys(),
+      R.intersection(R.keys(typescriptEslint.plugin.rules ?? {})),
+      R.mapToObj((ruleName) => [ruleName, 'off']),
+    ),
+  };
+}
 
 interface Options {
   tsconfigRootDir: string;
@@ -19,5 +33,6 @@ export function createTypescriptConfig(options: Options): Config[] {
         },
       },
     },
+    disableOverriddenRules(),
   ]);
 }
